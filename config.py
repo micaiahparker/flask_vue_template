9f661e280ac5dc3bdd7d11c3d018e81b36873eab
Micaiah Parker <me@micaiahparker.com>
2017-06-19 00:43:04 -0400

    fixed typo

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 from secrets import token_urlsafe
-
 from dotenv import load_dotenv, find_dotenv
 
-load_dotenv(find_dotenv)
+#load .env if available
+load_dotenv(find_dotenv())
 
 class Config:
     SECRET_KEY = token_urlsafe()
-    ENV = os.environ.copy()
+    ENV = os.environ.copy() #put .env in config
